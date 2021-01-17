94f2e37a18776d4aab5126d9bf58d3505f9c0888
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-17 19:59:48 +0100

    Upload register.py

---
+++ register.py
@@ -0,0 +1,178 @@
+import os
+import numpy as np
+from tqdm import tqdm
+
+Tcut_halogas = 1.e5  # Hot gas temperature threshold in K
+SILENT_PROGRESSBAR = False
+
+
+class Macsis:
+    name: str = 'MACSIS'
+    cosma_repository: str = '/cosma5/data/dp004/dc-hens1/macsis/macsis_gas'
+    output_dir: str = '/cosma6/data/dp004/dc-alta2/macsis_analysis'
+
+    def __init__(self) -> None:
+
+        # Sort halos by index number
+        halos_list = os.listdir(self.cosma_repository)
+        halos_list.sort(key=lambda x: int(x[-5:]))
+
+        # Load halos data directory and instances of Zoom
+        self.halo_paths = []
+        self.zooms_list = []
+
+        for zoom_dir in tqdm(halos_list, desc=f"Fetching zoom objects", disable=SILENT_PROGRESSBAR):
+            if zoom_dir.startswith('halo') and os.path.isdir(zoom_dir):
+                self.halo_paths.append(
+                    os.path.join(self.cosma_repository, zoom_dir)
+                )
+                self.zooms_list.append(
+                    Zoom(os.path.join(self.cosma_repository, zoom_dir))
+                )
+
+    def get_zoom(self, index: int):
+
+        try:
+            zoom_select = self.zooms_list[index]
+        except IndexError as err:
+            print((
+                f"Trying to access zoom object with output index {index:d}, "
+                f"but the maximum index available is {len(self.zooms_list) - 1:d}."
+            ))
+            raise err
+
+        return zoom_select
+
+
+class Redshift(object):
+
+    __slots__ = (
+        'scale_factor',
+        'a',
+        'redshift',
+        'z',
+        'snapshot_path',
+        'catalogue_grouptab_path',
+        'catalogue_subfindtab_path',
+        'catalogue_particles_path',
+    )
+
+    scale_factor: float
+    a: float
+    redshift: float
+    z: float
+    snapshot_path: str
+    catalogue_grouptab_path: str
+    catalogue_subfindtab_path: str
+    catalogue_particles_path: str
+
+    def __init__(self, info_dict: dict):
+        for key in info_dict:
+            setattr(self, key, info_dict[key])
+
+        setattr(self, 'a', self.scale_factor)
+        setattr(self, 'z', self.redshift)
+
+    def __str__(self):
+        return (
+            f"Scale factor (a):         {self.scale_factor}\n"
+            f"Redshift (z):             {self.redshift}\n"
+            f"Snapshot file:            {self.snapshot_path}\n"
+            f"Catalog group-tab file:   {self.catalogue_grouptab_path}\n"
+            f"Catalog subfind-tab file: {self.catalogue_subfindtab_path}\n"
+            f"Catalog particles file:   {self.catalogue_particles_path}"
+        )
+
+
+class Zoom(object):
+
+    def __init__(self, run_directory: str) -> None:
+        self.run_name = os.path.basename(run_directory)
+        self.run_directory = run_directory
+        self.scale_factors, self.redshifts = self.read_output_list()
+
+        # Retrieve complete data paths to files
+        self.snapshot_paths = []
+        self.catalogue_grouptab_paths = []
+        self.catalogue_subfindtab_paths = []
+        self.catalogue_particles_paths = []
+
+        for dir_output in os.listdir(os.path.join(run_directory, 'data')):
+            path_output = os.path.join(run_directory, 'data', dir_output)
+
+            # Retrieve snapshots file paths
+            if dir_output.startswith('snapshot'):
+
+                files = os.listdir(path_output)
+                if len(files) == 1 and isinstance(files, list):
+                    files = os.path.join(path_output, files[0])
+                else:
+                    files = tuple([os.path.join(path_output, file) for file in files])
+
+                self.snapshot_paths.append(files)
+
+            # Retrieve group_tab and subfind_tab files
+            elif dir_output.startswith('groups'):
+
+                files = os.listdir(path_output)
+                for file in files:
+                    if file.startswith('eagle_subfind_tab'):
+                        self.catalogue_subfindtab_paths.append(os.path.join(path_output, file))
+                    elif file.startswith('group_tab'):
+                        self.catalogue_grouptab_paths.append(os.path.join(path_output, file))
+
+            # Retrieve subfind particle data
+            elif dir_output.startswith('particledata'):
+
+                files = os.listdir(path_output)
+                if len(files) == 1 and isinstance(files, list):
+                    files = os.path.join(path_output, files[0])
+                else:
+                    files = tuple([os.path.join(path_output, file) for file in files])
+
+                self.catalogue_particles_paths.append(files)
+
+        assert len(self.scale_factors) == len(self.snapshot_paths)
+        assert len(self.scale_factors) == len(self.catalogue_subfindtab_paths)
+        assert len(self.scale_factors) == len(self.catalogue_grouptab_paths)
+        assert len(self.scale_factors) == len(self.catalogue_particles_paths)
+
+    def read_output_list(self):
+        output_list_file = os.path.join(self.run_directory, 'output_list')
+        scale_factors = np.genfromtxt(output_list_file)
+        redshifts = 1 / scale_factors - 1
+        return scale_factors, redshifts
+
+    def get_redshift(self, index: int = 0):
+
+        try:
+            redshift_select = self.redshifts[index]
+        except IndexError as err:
+            print((
+                f"Trying to access redshift with output index {index:d}, "
+                f"but the maximum index available is {len(self.redshifts) - 1:d}."
+            ))
+            raise err
+
+        redshift_info = dict()
+        redshift_info['scale_factor'] = self.scale_factors[index]
+        redshift_info['redshift'] = redshift_select
+        redshift_info['snapshot_path'] = self.snapshot_paths[index]
+        redshift_info['catalogue_grouptab_path'] = self.catalogue_subfindtab_paths[index]
+        redshift_info['catalogue_subfindtab_path'] = self.catalogue_grouptab_paths[index]
+        redshift_info['catalogue_particles_path'] = self.catalogue_particles_paths[index]
+
+        assert f"{index:03d}" in os.path.basename(redshift_info['snapshot_path'])
+        assert f"{index:03d}" in os.path.basename(redshift_info['catalogue_grouptab_path'])
+        assert f"{index:03d}" in os.path.basename(redshift_info['catalogue_subfindtab_path'])
+        assert f"{index:03d}" in os.path.basename(redshift_info['catalogue_particles_path'])
+
+        return Redshift(redshift_info)
+
+
+if __name__ == "__main__":
+    macsis = Macsis()
+    print('Halo list:', macsis.halo_paths)
+
+    halo0 = macsis.get_zoom(0).get_redshift(22)
+    print(halo0)
---
+++ utils.py
@@ -0,0 +1,39 @@
+import sys
+import os
+from types import ModuleType, FunctionType
+from gc import get_referents
+import psutil
+
+total_memory = psutil.virtual_memory().total
+
+# Custom objects know their class.
+# Function objects seem to know way too much, including modules.
+# Exclude modules as well.
+BLACKLIST = type, ModuleType, FunctionType
+
+
+def getsize(obj):
+    """sum size of object & members."""
+    if isinstance(obj, BLACKLIST):
+        raise TypeError('getsize() does not take argument of type: ' + str(type(obj)))
+    seen_ids = set()
+    size = 0
+    objects = [obj]
+    while objects:
+        need_referents = []
+        for obj in objects:
+            if not isinstance(obj, BLACKLIST) and id(obj) not in seen_ids:
+                seen_ids.add(id(obj))
+                size += sys.getsizeof(obj)
+                need_referents.append(obj)
+        objects = get_referents(*need_referents)
+    return size
+
+
+def dump_memory_usage() -> None:
+    process = psutil.Process(os.getpid())
+    memory = process.memory_info().rss  # in bytes
+    print((
+        f"[Resources] Memory usage: {memory / 1024 / 1024:.2f} MB "
+        f"({memory / total_memory * 100:.2f}%)"
+    ))
