3f7b8a1455862d4bc4c8cd29a7b60b7a6ac5b9cd
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-18 12:49:19 +0100

    Upload register

--- maps/rksz.py
+++ maps/rksz.py
@@ -63,98 +63,110 @@ def rotate_velocities(vel: np.ndarray, angular_momentum_hot_gas: np.ndarray, til
     return new_vel
 
 
+def rksz_map(halo):
+    data = MacsisDataset(halo)
+
+    # Read data
+    coordinates = data.read_snapshot('PartType0/Coordinates')
+    densities = data.read_snapshot('PartType0/Density')
+    masses = data.read_snapshot('PartType0/Mass')
+    velocities = data.read_snapshot('PartType0/Velocity')
+    temperatures = data.read_snapshot('PartType0/Temperature')
+    smoothing_lengths = data.read_snapshot('PartType0/SmoothingLength')
+
+    # Remember that the largest FOF has index 1
+    centre_of_potential = data.read_catalogue_subfindtab('FOF/GroupCentreOfPotential')[1]
+    r500_crit = data.read_catalogue_subfindtab('FOF/Group_R_Crit500')[1]
+    m500_crit = data.read_catalogue_subfindtab('FOF/Group_M_Crit500')[1]
+
+    # Select ionised hot gas
+    temperature_cut = np.where(temperatures > 1.e5)[0]
+
+    coordinates = coordinates[temperature_cut]
+    densities = densities[temperature_cut]
+    masses = masses[temperature_cut]
+    velocities = velocities[temperature_cut]
+    temperatures = temperatures[temperature_cut]
+    smoothing_lengths = smoothing_lengths[temperature_cut]
+
+    # Rescale coordinates to CoP
+    coordinates[:, 0] -= centre_of_potential[0]
+    coordinates[:, 1] -= centre_of_potential[1]
+    coordinates[:, 2] -= centre_of_potential[2]
+
+    # Compute mean velocity inside R500
+    radial_dist = np.sqrt(
+        coordinates[:, 0] ** 2 +
+        coordinates[:, 1] ** 2 +
+        coordinates[:, 2] ** 2
+    )
+    r500_mask = np.where(radial_dist < r500_crit)[0]
+
+    mean_velocity_r500 = np.sum(velocities[r500_mask] * masses[r500_mask, None], axis=0) / np.sum(masses[r500_mask])
+    angular_momentum_r500 = np.sum(
+        np.cross(coordinates[r500_mask], velocities[r500_mask] * masses[r500_mask, None]), axis=0
+    ) / np.sum(masses[r500_mask])
+
+    velocities_rest_frame = velocities.copy()
+    velocities_rest_frame[:, 0] -= mean_velocity_r500[0]
+    velocities_rest_frame[:, 1] -= mean_velocity_r500[1]
+    velocities_rest_frame[:, 2] -= mean_velocity_r500[2]
+
+    # Rotate coordinates and velocities
+    coordinates_edgeon = rotate_coordinates(coordinates, angular_momentum_r500, tilt='edgeon')
+    velocities_rest_frame_edgeon = rotate_velocities(velocities_rest_frame, angular_momentum_r500, tilt='edgeon')
+
+    # Rotate angular momentum vector for cross check
+    angular_momentum_r500_rotated = rotate_coordinates(
+        angular_momentum_r500 / np.linalg.norm(angular_momentum_r500), angular_momentum_r500, tilt='edgeon'
+    ) * r500_crit / 2
+
+    compton_y = unyt.unyt_array(
+        masses * velocities_rest_frame_edgeon[:, 2], 1.e10 * unyt.Solar_Mass * 1.e3 * unyt.km / unyt.s
+    ) * ksz_const / unyt.unyt_quantity(1., unyt.Mpc) ** 2
+    compton_y = compton_y.value
+
+    # Restrict map to 2*R500
+    spatial_filter = np.where(
+        (np.abs(coordinates_edgeon[:, 0]) < r500_crit / 2) &
+        (np.abs(coordinates_edgeon[:, 1]) < r500_crit / 2) &
+        (np.abs(coordinates_edgeon[:, 2]) < r500_crit / 2)
+    )[0]
+
+    coordinates_edgeon = coordinates_edgeon[spatial_filter]
+    velocities_rest_frame_edgeon = velocities_rest_frame_edgeon[spatial_filter]
+    smoothing_lengths = smoothing_lengths[spatial_filter]
+    compton_y = compton_y[spatial_filter]
+
+    # Make map using swiftsimio
+    x = (coordinates_edgeon[:, 0] - coordinates_edgeon[:, 0].min()) / (
+            coordinates_edgeon[:, 0].max() - coordinates_edgeon[:, 0].min())
+    y = (coordinates_edgeon[:, 1] - coordinates_edgeon[:, 1].min()) / (
+            coordinates_edgeon[:, 1].max() - coordinates_edgeon[:, 1].min())
+    h = smoothing_lengths / (coordinates_edgeon.max() - coordinates_edgeon.min()) ** 2
+
+    # Gather and handle coordinates to be processed
+    x = np.asarray(x, dtype=np.float64)
+    y = np.asarray(y, dtype=np.float64)
+    m = np.asarray(compton_y, dtype=np.float32)
+    h = np.asarray(h, dtype=np.float32)
+    res = 1024
+    smoothed_map = scatter(x=x, y=y, m=m, h=h, res=res).T
+    print(compton_y.max(), compton_y.min(), smoothing_lengths.max(), smoothed_map.min())
+
+    return smoothed_map
+
+
 macsis = Macsis()
 
-halo_handle = macsis.get_zoom(0).get_redshift(-1)
-data = MacsisDataset(halo_handle)
+for zoom_id in range(30):
+    print("Zoom ID:", zoom_id)
+    halo_handle = macsis.get_zoom(zoom_id).get_redshift(-1)
+    if zoom_id == 0:
+        smoothed_map = rksz_map(halo_handle)
+    else:
+        smoothed_map += rksz_map(halo_handle)
 
-# Read data
-coordinates = data.read_snapshot('PartType0/Coordinates')
-densities = data.read_snapshot('PartType0/Density')
-masses = data.read_snapshot('PartType0/Mass')
-velocities = data.read_snapshot('PartType0/Velocity')
-temperatures = data.read_snapshot('PartType0/Temperature')
-smoothing_lengths = data.read_snapshot('PartType0/SmoothingLength')
-
-# Remember that the largest FOF has index 1
-centre_of_potential = data.read_catalogue_subfindtab('FOF/GroupCentreOfPotential')[1]
-r500_crit = data.read_catalogue_subfindtab('FOF/Group_R_Crit500')[1]
-m500_crit = data.read_catalogue_subfindtab('FOF/Group_M_Crit500')[1]
-
-# Select ionised hot gas
-temperature_cut = np.where(temperatures > 1.e5)[0]
-
-coordinates = coordinates[temperature_cut]
-densities = densities[temperature_cut]
-masses = masses[temperature_cut]
-velocities = velocities[temperature_cut]
-temperatures = temperatures[temperature_cut]
-smoothing_lengths = smoothing_lengths[temperature_cut]
-
-# Rescale coordinates to CoP
-coordinates[:, 0] -= centre_of_potential[0]
-coordinates[:, 1] -= centre_of_potential[1]
-coordinates[:, 2] -= centre_of_potential[2]
-
-# Compute mean velocity inside R500
-radial_dist = np.sqrt(
-    coordinates[:, 0] ** 2 +
-    coordinates[:, 1] ** 2 +
-    coordinates[:, 2] ** 2
-)
-r500_mask = np.where(radial_dist < r500_crit)[0]
-
-mean_velocity_r500 = np.sum(velocities[r500_mask] * masses[r500_mask, None], axis=0) / np.sum(masses[r500_mask])
-angular_momentum_r500 = np.sum(
-    np.cross(coordinates[r500_mask], velocities[r500_mask] * masses[r500_mask, None]), axis=0
-) / np.sum(masses[r500_mask])
-
-velocities_rest_frame = velocities.copy()
-velocities_rest_frame[:, 0] -= mean_velocity_r500[0]
-velocities_rest_frame[:, 1] -= mean_velocity_r500[1]
-velocities_rest_frame[:, 2] -= mean_velocity_r500[2]
-
-# Rotate coordinates and velocities
-coordinates_edgeon = rotate_coordinates(coordinates, angular_momentum_r500, tilt='edgeon')
-velocities_rest_frame_edgeon = rotate_velocities(velocities_rest_frame, angular_momentum_r500, tilt='edgeon')
-
-# Rotate angular momentum vector for cross check
-angular_momentum_r500_rotated = rotate_coordinates(
-    angular_momentum_r500 / np.linalg.norm(angular_momentum_r500), angular_momentum_r500, tilt='edgeon'
-) * r500_crit / 2
-
-compton_y = unyt.unyt_array(
-    masses * velocities_rest_frame_edgeon[:, 2], 1.e10 * unyt.Solar_Mass * 1.e3 * unyt.km / unyt.s
-) * ksz_const / unyt.unyt_quantity(1., unyt.Mpc) ** 2
-compton_y = compton_y.value
-
-# Restrict map to 2*R500
-spatial_filter = np.where(
-    (np.abs(coordinates_edgeon[:, 0]) < r500_crit / 2) &
-    (np.abs(coordinates_edgeon[:, 1]) < r500_crit / 2) &
-    (np.abs(coordinates_edgeon[:, 2]) < r500_crit / 2)
-)[0]
-
-coordinates_edgeon = coordinates_edgeon[spatial_filter]
-velocities_rest_frame_edgeon = velocities_rest_frame_edgeon[spatial_filter]
-smoothing_lengths = smoothing_lengths[spatial_filter]
-compton_y = compton_y[spatial_filter]
-
-# Make map using swiftsimio
-x = (coordinates_edgeon[:, 0] - coordinates_edgeon[:, 0].min()) / (
-        coordinates_edgeon[:, 0].max() - coordinates_edgeon[:, 0].min())
-y = (coordinates_edgeon[:, 1] - coordinates_edgeon[:, 1].min()) / (
-        coordinates_edgeon[:, 1].max() - coordinates_edgeon[:, 1].min())
-h = smoothing_lengths / (coordinates_edgeon.max() - coordinates_edgeon.min()) ** 2
-
-# Gather and handle coordinates to be processed
-x = np.asarray(x, dtype=np.float64)
-y = np.asarray(y, dtype=np.float64)
-m = np.asarray(compton_y, dtype=np.float32)
-h = np.asarray(h, dtype=np.float32)
-res = 1024
-smoothed_map = scatter(x=x, y=y, m=m, h=h, res=res).T
-print(compton_y.max(), compton_y.min(), smoothing_lengths.max(), smoothed_map.min())
 # smoothed_map = np.ma.masked_where(np.log10(np.abs(smoothed_map)) < -20, smoothed_map)
 vlim = np.abs(smoothed_map).max()
 plt.imshow(
@@ -162,23 +174,23 @@ plt.imshow(
     norm=SymLogNorm(linthresh=0.01, linscale=1, vmin=-vlim, vmax=vlim),
     cmap="PRGn",
     origin="lower",
-    extent=(
-        coordinates_edgeon[:, 0].min(),
-        coordinates_edgeon[:, 0].max(),
-        coordinates_edgeon[:, 1].min(),
-        coordinates_edgeon[:, 1].max(),
-    )
+    # extent=(
+    #     coordinates_edgeon[:, 0].min(),
+    #     coordinates_edgeon[:, 0].max(),
+    #     coordinates_edgeon[:, 1].min(),
+    #     coordinates_edgeon[:, 1].max(),
+    # )
 )
-plt.plot([0, angular_momentum_r500_rotated[0]], [0, angular_momentum_r500_rotated[1]], marker='o')
+# plt.plot([0, angular_momentum_r500_rotated[0]], [0, angular_momentum_r500_rotated[1]], marker='o')
 plt.axis('off')
-# plt.show()
+plt.show()
 plt.close()
 
-spectrum = np.fft.fftshift(np.fft.fft2(smoothed_map))
-freqx = np.fft.fftshift(np.fft.fftfreq(res, 1))
-freqy = np.fft.fftshift(np.fft.fftfreq(res, 1))
-fX, fY = np.meshgrid(freqx, freqy)
-plt.pcolormesh(fX, fY, np.abs(spectrum), norm=LogNorm())
-plt.xlim([-0.01, 0.01])
-plt.ylim([-0.01, 0.01])
-plt.show()
+# spectrum = np.fft.fftshift(np.fft.fft2(smoothed_map))
+# freqx = np.fft.fftshift(np.fft.fftfreq(res, 1))
+# freqy = np.fft.fftshift(np.fft.fftfreq(res, 1))
+# fX, fY = np.meshgrid(freqx, freqy)
+# plt.pcolormesh(fX, fY, np.abs(spectrum), norm=LogNorm())
+# plt.xlim([-0.01, 0.01])
+# plt.ylim([-0.01, 0.01])
+# plt.show()
