be225ae960296482c0267bffe85d4fc887b6be28
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-18 23:41:24 +0100

    Upload register

--- maps/rksz.py
+++ maps/rksz.py
@@ -164,17 +164,22 @@ def dump_to_hdf5_parallel():
 
     with h5py.File('rksz_gas.hdf5', 'w', driver='mpio', comm=MPI.COMM_WORLD) as f:
 
-
+        # Editing the structure of the file MUST be done collectively
+        zoom_handles = []
         for zoom_id in range(60):
+            data_handle = macsis.get_zoom(zoom_id).get_redshift(-1)
+            # Create group containing all halo data
+            halo_group = f.create_group(f"{data_handle.run_name}")
+            rksz = rksz_map(data_handle, resolution=1024, alignment='edgeon')
+            halo_group.create_dataset(f"gas_rksz_edgeon", rksz.shape, dtype=rksz.dtype)
+            zoom_handles.append(data_handle)
+
+        for zoom_id, data_handle in enumerate(zoom_handles):
 
             if zoom_id % num_processes == rank:
-                data_handle = macsis.get_zoom(zoom_id).get_redshift(-1)
                 print(f"Rank {rank} processing halo {zoom_id} (MACSIS name {data_handle.run_name})")
-
-                # Create group containing all halo data
-                halo_group = f.create_group(f"{data_handle.run_name}")
-                # rksz = rksz_map(data_handle, resolution=1024, alignment='edgeon')
-                # halo_group.create_dataset(f"gas_rksz_edgeon", rksz.shape, dtype=rksz.dtype, data=rksz)
+                rksz = rksz_map(data_handle, resolution=1024, alignment='edgeon')
+                f[f"{data_handle.run_name}/gas_rksz_edgeon"][:] = rksz
 
 
 dump_to_hdf5_parallel()
