8ed56fbccc6c59687f2811c59c2b46788be9bf32
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-19 12:18:21 +0100

    Upload register

--- maps/rksz.py
+++ maps/rksz.py
@@ -168,7 +168,7 @@ def dump_to_hdf5_parallel():
     with h5py.File('rksz_gas.hdf5', 'w', driver='mpio', comm=comm) as f:
 
         # Retrieve all zoom handles in parallel (slow otherwise)
-        data_handles = np.empty((macsis.num_zooms // num_processes + 1, num_processes), dtype=np.object)
+        data_handles = np.empty((num_processes, macsis.num_zooms // num_processes + 1), dtype=np.object)
         for zoom_id in range(macsis.num_zooms):
             if zoom_id % num_processes == rank:
                 print(f"Collecting metadata for process ({zoom_id}/{macsis.num_zooms - 1})...")
@@ -176,8 +176,8 @@ def dump_to_hdf5_parallel():
 
         # print(data_handle)
         zoom_handles = comm.alltoall(data_handles)
-        if rank == 0:
-            print(zoom_handles)
+
+        print(zoom_handles)
         zoom_handles = np.concatenate(zoom_handles).ravel()
 
         # Editing the structure of the file MUST be done collectively
