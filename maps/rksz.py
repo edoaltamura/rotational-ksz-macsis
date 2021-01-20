c767f52fc62b404805bcaf91faa56d8be956486c
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-20 10:21:08 +0100

    Upload register

--- maps/rksz.py
+++ maps/rksz.py
@@ -327,7 +327,7 @@ def dump_dm_to_hdf5_parallel():
 if rank == 0:
     with h5py.File(f'{Macsis().output_dir}/rksz_gas.hdf5', 'r') as f:
         for i, halo in enumerate(f.keys()):
-            dataset = f[f"{halo}/map_edgeon"][:]
+            dataset = f[f"{halo}/map_faceon"][:]
             print((
                 f"Merging map from {halo} | "
                 f"shape: {dataset.shape} | "
